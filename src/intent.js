d3af39ad5c005222994d57f750b8c223e7e0ef52
bouzuya <m@bouzuya.net>
2015-10-30 19:11:08 +0900

    remove updateIssue action

--- src/intent.js
+++ src/intent.js
@@ -1,11 +1,9 @@
 import fetchIssues from './actions/fetch-issues';
-import updateIssue from './actions/update-issue';
 import updateIssues from './actions/update-issues';
 
 export default function(responses) {
   const actions = {
     ...fetchIssues(responses),
-    ...updateIssue(responses),
     ...updateIssues(responses)
   };
   return actions;
