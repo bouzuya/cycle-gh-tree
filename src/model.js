fad7ab22a5c611c57bdec1021b1117c7ff97e5e0
bouzuya <m@bouzuya.net>
2015-10-30 20:19:31 +0900

    add multi repos support

--- src/model.js
+++ src/model.js
@@ -18,13 +18,16 @@ export default function(actions) {
     .scan((state, action) => action(state))
     .share();
   state$.request$ = fetchIssues$
-  .map(() => {
-    const baseUrl = 'https://api.github.com';
-    const owner = 'bouzuya';
-    const repo = 'blog.bouzuya.net';
-    const url = `${baseUrl}/repos/${owner}/${repo}/issues`;
-    const headers = { 'User-Agent': 'gh-tree' };
-    return { method: 'GET', url, headers };
+  .flatMap(() => {
+    const repos = [
+      { user: 'bouzuya', repo: 'bouzuya.net' },
+      { user: 'bouzuya', repo: 'blog.bouzuya.net' }
+    ];
+    return Rx.Observable.from(repos.map(({ user, repo }) => {
+      const url = `https://api.github.com/repos/${user}/${repo}/issues`;
+      const headers = { 'User-Agent': 'gh-tree' };
+      return { method: 'GET', url, headers };
+    }));
   });
   return state$;
 }
