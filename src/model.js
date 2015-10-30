56a224ee7925620144f6b6333c2cf45e2987bfd8
bouzuya <m@bouzuya.net>
2015-10-30 19:22:29 +0900

    remove state.requests

--- src/model.js
+++ src/model.js
@@ -6,29 +6,12 @@ export default function(actions) {
     updateIssues$
   } = actions;
   const state = {
-    message: 'Hello, world!',
-    count: 0,
     issues: [],
-    requests: []
+    message: 'Hello, world!'
   };
   const actions$ = Rx.Observable.merge(
-    fetchIssues$
-    .map(() => {
-      const baseUrl = 'https://api.github.com';
-      const owner = 'bouzuya';
-      const repo = 'blog.bouzuya.net';
-      const url = `${baseUrl}/repos/${owner}/${repo}/issues`;
-      const headers = { 'User-Agent': 'gh-tree' };
-      return { method: 'GET', url, headers };
-    })
-    .map(request => state => {
-      state.count += 1;
-      state.requests.push(request); // FIXME
-      return state;
-    }),
     updateIssues$
     .map(({ issues }) => state => {
-      state.requests.shift(); // FIXME
       state.issues = issues;
       state.message = 'fetched!';
       return state;
@@ -38,5 +21,14 @@ export default function(actions) {
     .merge(actions$)
     .scan((state, action) => action(state))
     .share();
+  state$.request$ = fetchIssues$
+  .map(() => {
+    const baseUrl = 'https://api.github.com';
+    const owner = 'bouzuya';
+    const repo = 'blog.bouzuya.net';
+    const url = `${baseUrl}/repos/${owner}/${repo}/issues`;
+    const headers = { 'User-Agent': 'gh-tree' };
+    return { method: 'GET', url, headers };
+  });
   return state$;
 }
--- src/view.js
+++ src/view.js
@@ -2,10 +2,9 @@ import { h } from '@cycle/dom';
 import Rx from 'rx';
 
 export default function(state$) {
-  const vtree$ = state$.map(({ message, count, issues }) => {
+  const vtree$ = state$.map(({ message, issues }) => {
     return h('div', [
       h('button', ['fetch']),
-      '' + count,
       message,
       h('ul', issues.map(({ url, title, number }) => {
         return h('li', [
@@ -14,9 +13,7 @@ export default function(state$) {
       }))
     ])
   });
-  const request$ = state$.flatMap(({ requests }) => {
-    return Rx.Observable.from(requests);
-  }).filter(i => i);
+  const request$ = state$.request$;
   const requests = {
     DOM: vtree$,
     HTTP: request$
