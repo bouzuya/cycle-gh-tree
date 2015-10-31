66837a9b7b0e92cc1303d681a8ed21b7326ed04a
bouzuya <m@bouzuya.net>
2015-10-31 22:24:02 +0900

    add validation for repo

--- src/model.js
+++ src/model.js
@@ -15,11 +15,11 @@ export default function(actions) {
     repo: null,
     user: null
   };
+  const reposMaxLength = 10;
   const actions$ = Rx.Observable.merge(
     fetchIssues$
     .map((_, i) => state => {
       const newRequests = state.repos.map(({ user, repo }, j) => {
-        const reposMaxLength = 10;
         const id = i * reposMaxLength + j;
         const url = `https://api.github.com/repos/${user}/${repo}/issues`;
         const headers = { 'User-Agent': 'gh-tree' };
@@ -31,6 +31,11 @@ export default function(actions) {
     addRepo$
     .map(repo => state => {
       const { user, repo } = state;
+      if (state.repos.length > reposMaxLength) return state;
+      const duplicated = state.repos.filter(i => {
+        return i.user === user && i.repo === repo;
+      }).length > 0;
+      if (duplicated) return state;
       state.repos.push({ user, repo });
       return state;
     }),
