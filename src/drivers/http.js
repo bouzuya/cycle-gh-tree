f9a52d95fba6b9aff73e2c6faf3ecb0e24c467c1
bouzuya <m@bouzuya.net>
2015-11-01 23:13:50 +0900

    use distinct

--- src/drivers/http.js
+++ src/drivers/http.js
@@ -2,17 +2,10 @@ import fetch from 'node-fetch';
 import Rx from 'rx';
 
 function makeHTTPDriver() {
-  var requests = {};
   return function(request$) {
     return request$
-    .filter(request => {
-      if (typeof request === 'undefined' || request === null) return false;
-      const { id } = request;
-      if (typeof id === 'undefined' || id === null) return false;
-      if (requests.hasOwnProperty(id)) return false;
-      requests[id] = request;
-      return true;
-    })
+    .filter(request => !(typeof request === 'undefined' || request === null))
+    .distinct()
     .map(request => {
       const { url, method, body, headers } = request;
       const promise = fetch(url, { method, body, headers });
