0ee39bb9b8f046340597df179b99ece15fe55ef9
bouzuya <m@bouzuya.net>
2015-11-05 21:35:10 +0900

    add user/repo to list view

--- src/view.js
+++ src/view.js
@@ -15,15 +15,15 @@ export default function(state$) {
         return h('li', [user, '/', repo]);
       })),
       h('button.fetch', ['fetch']),
-      h('ul', issues.map(({ url, title, number, children }) => {
+      h('ul', issues.map(({ user, repo, url, title, number, children }) => {
         return h('li', [
-          h('a', { href: url }, [`#${number}`]),
+          h('a', { href: url }, [`${user}/${repo}#${number}`]),
           ' ',
           title,
           children.length === 0 ? null : h('ul', children.map(i => {
-            const { url, title, number } = i;
+            const { user, repo, url, title, number } = i;
             return h('li', [
-              h('a', { href: url }, [`#${number}`]),
+              h('a', { href: url }, [`${user}/${repo}#${number}`]),
               ' ',
               title
             ]);
