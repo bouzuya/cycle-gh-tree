7bd6f152fd34f434ea3ee5d8fe9f13b475a2834f
bouzuya <m@bouzuya.net>
2015-11-15 17:51:45 +0900

    extract transforms/issues

--- src/model.js
+++ src/model.js
@@ -1,4 +1,5 @@
 import Rx from 'rx';
+import issues from './transforms/issues';
 import repos from './transforms/repos';
 import token from './transforms/token';
 
@@ -22,56 +23,8 @@ function initializeToken() {
   };
 }
 
-function indexOf(issues, issue) {
-  const indexes = issues
-  .map((i, index) => [i, index])
-  .filter(([i, _]) => i.url === issue.url)
-  .map(([_, index]) => index);
-  return indexes.length > 0 ? indexes[0] : -1;
-}
-
-function merge(issues, issue) {
-  const index = indexOf(issues, issue);
-  if (index >= 0) {
-    issues[index] = issue;
-  } else {
-    issues.push(issue);
-  }
-  return addChildren(addParent(issues));
-}
-
-function addParent(issues) {
-  return issues.map(issue => {
-    const m = (issue.body || '').match(/^(?:([^\/]+)\/([^#]+))?#(\d+)/);
-    if (!m) return Object.assign({}, issue, { parent: null });
-    const [_, u, r, n] = m;
-    return Object.assign({}, issue, {
-      parent: {
-        user: u || issue.user,
-        repo: r || issue.repo,
-        number: parseInt(n, 10)
-      }
-    });
-  });
-}
-
-function addChildren(issues) {
-  return issues.map(issue => {
-    const { user, repo, number } = issue;
-    const children = issues.filter(i => {
-      const p = i.parent;
-      return p && p.user === user && p.repo === repo && p.number === number;
-    });
-    return Object.assign({}, issue, { children });
-  });
-}
-
 export default function(actions) {
-  const {
-    loadSettings$,
-    fetchIssues$,
-    updateIssue$,
-  } = actions;
+  const { loadSettings$ } = actions;
   const state = {
     issues: [],
     requests: [],
@@ -81,34 +34,13 @@ export default function(actions) {
   const reposMaxLength = 10;
   const actions$ = Rx.Observable.merge(
     loadSettings$
-    .map(storage => state => {
-      state.settings = storage;
-      return state;
-    }),
-    fetchIssues$
-    .map((_, i) => state => {
-      const { settings } = state;
-      const repos = settings && settings.repos ? settings.repos : [];
-      const newRequests = repos.map(({ user, repo }, j) => {
-        const id = i * reposMaxLength + j;
-        const url = `https://api.github.com/repos/${user}/${repo}/issues`;
-        const ua = { 'User-Agent': 'gh-tree' };
-        const auth = settings.token ? {
-          'Authorization': `token ${settings.token}`
-        } : {};
-        const headers = Object.assign({}, ua, auth);
-        return { id, method: 'GET', url, headers };
-      });
-      state.requests = state.requests.concat(newRequests);
-      return state;
-    }),
+      .map(storage => state => {
+        state.settings = storage;
+        return state;
+      }),
+    issues(actions, { reposMaxLength }),
     repos(actions, { reposMaxLength }),
-    token(actions),
-    updateIssue$
-    .map(issue => state => {
-      state.issues = merge(state.issues, issue);
-      return state;
-    })
+    token(actions)
   );
   const state$ = Rx.Observable.just(state)
     .merge(actions$)
---
+++ src/transforms/issues.js
@@ -0,0 +1,81 @@
+import Rx from 'rx';
+
+function indexOf(issues, issue) {
+  const indexes = issues
+  .map((i, index) => [i, index])
+  .filter(([i, _]) => i.url === issue.url)
+  .map(([_, index]) => index);
+  return indexes.length > 0 ? indexes[0] : -1;
+}
+
+function merge(issues, issue) {
+  const index = indexOf(issues, issue);
+  if (index >= 0) {
+    issues[index] = issue;
+  } else {
+    issues.push(issue);
+  }
+  return addChildren(addParent(issues));
+}
+
+function addParent(issues) {
+  return issues.map(issue => {
+    const m = (issue.body || '').match(/^(?:([^\/]+)\/([^#]+))?#(\d+)/);
+    if (!m) return Object.assign({}, issue, { parent: null });
+    const [_, u, r, n] = m;
+    return Object.assign({}, issue, {
+      parent: {
+        user: u || issue.user,
+        repo: r || issue.repo,
+        number: parseInt(n, 10)
+      }
+    });
+  });
+}
+
+function addChildren(issues) {
+  return issues.map(issue => {
+    const { user, repo, number } = issue;
+    const children = issues.filter(i => {
+      const p = i.parent;
+      return p && p.user === user && p.repo === repo && p.number === number;
+    });
+    return Object.assign({}, issue, { children });
+  });
+}
+
+function fetchIssuesTransform({ fetchIssues$ }, { reposMaxLength }) {
+  return fetchIssues$
+    .map((_, i) => state => {
+      const { settings } = state;
+      const repos = settings && settings.repos ? settings.repos : [];
+      const newRequests = repos.map(({ user, repo }, j) => {
+        const id = i * reposMaxLength + j;
+        const url = `https://api.github.com/repos/${user}/${repo}/issues`;
+        const ua = { 'User-Agent': 'gh-tree' };
+        const auth = settings.token ? {
+          'Authorization': `token ${settings.token}`
+        } : {};
+        const headers = Object.assign({}, ua, auth);
+        return { id, method: 'GET', url, headers };
+      });
+      state.requests = state.requests.concat(newRequests);
+      return state;
+    });
+}
+
+function updateIssueTransform({ updateIssue$ }) {
+  return updateIssue$
+    .map(issue => state => {
+      state.issues = merge(state.issues, issue);
+      return state;
+    });
+}
+
+export default function(actions, { reposMaxLength }) {
+  // NOTE: no namespace
+  return Rx.Observable.merge(
+    fetchIssuesTransform(actions, { reposMaxLength }),
+    updateIssueTransform(actions),
+  );
+}
