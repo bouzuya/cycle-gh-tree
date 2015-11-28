9d5c061df9d93d438cc5c6959180ba35bf4e8de0
bouzuya <bouzuya@storm.local>
2015-11-28 22:14:03 +0900

    add milestone filter feature

--- src/actions/add-filter.js
+++ src/actions/add-filter.js
@@ -1,5 +1,7 @@
-export default function({ DOM }) {
-  const addFilter$ = DOM
+import { Observable } from 'rx';
+
+function addLabelFilter({ DOM }) {
+  return DOM
     .select('.filter-label input[type=checkbox]')
     .events('click')
     .filter(e => e.target.checked)
@@ -8,5 +10,24 @@ export default function({ DOM }) {
       const name = e.target.value;
       return { type, name };
     });
+}
+
+function addMilestoneFilter({ DOM }) {
+  return DOM
+    .select('.filter-milestone input[type=checkbox]')
+    .events('click')
+    .filter(e => e.target.checked)
+    .map(e => {
+      const type = 'milestone';
+      const name = e.target.value;
+      return { type, name };
+    });
+}
+
+export default function(responses) {
+  const addFilter$ = Observable.merge(
+    addLabelFilter(responses),
+    addMilestoneFilter(responses)
+  );
   return { addFilter$ };
 }
--- src/actions/remove-filter.js
+++ src/actions/remove-filter.js
@@ -1,5 +1,7 @@
-export default function({ DOM }) {
-  const removeFilter$ = DOM
+import { Observable } from 'rx';
+
+function removeLabelFilter({ DOM }) {
+  return DOM
     .select('.filter-label input[type=checkbox]')
     .events('click')
     .filter(e => !e.target.checked)
@@ -8,5 +10,24 @@ export default function({ DOM }) {
       const name = e.target.value;
       return { type, name };
     });
+}
+
+function removeMilestoneFilter({ DOM }) {
+  return DOM
+    .select('.filter-milestone input[type=checkbox]')
+    .events('click')
+    .filter(e => !e.target.checked)
+    .map(e => {
+      const type = 'milestone';
+      const name = e.target.value;
+      return { type, name };
+    });
+}
+
+export default function(responses) {
+  const removeFilter$ = Observable.merge(
+    removeLabelFilter(responses),
+    removeMilestoneFilter(responses)
+  );
   return { removeFilter$ };
 }
