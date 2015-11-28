f5778b3713eeac60b23342a434b5507ac0d43245
bouzuya <m@bouzuya.net>
2015-11-28 12:01:57 +0900

    add fetchLabels transform

--- src/model.js
+++ src/model.js
@@ -3,6 +3,7 @@ import issues from './transforms/issues';
 import repos from './transforms/repos';
 import token from './transforms/token';
 import assign from './utils/assign';
+import newRequests from './utils/new-requests';
 
 function initializeRepos() {
   return {
@@ -24,16 +25,36 @@ function initializeToken() {
   };
 }
 
+function newLabelRequest({ user, repo, token }) {
+  const url = `https://api.github.com/repos/${user}/${repo}/labels`;
+  const ua = { 'User-Agent': 'gh-tree' };
+  const auth = token ? { 'Authorization': `token ${token}` } : {};
+  const headers = assign({}, ua, auth);
+  return { method: 'GET', url, headers };
+}
+
 function labels(actions) {
-  const { updateLabel$ } = actions;
-  return updateLabel$
-    .map(label => state => {
-      const { labels } = state;
-      const index = labels.indexOf(label);
-      if (index >= 0) return state;
-      const newLabels = labels.concat([label]);
-      return assign({}, state, { labels: newLabels });
-    });
+  const { updateLabel$, fetchLabels$ } = actions;
+  return Rx.Observable.merge(
+    fetchLabels$
+      .map(() => state => {
+        const { settings, requests } = state;
+        const repos = settings && settings.repos ? settings.repos : [];
+        const token = settings.token;
+        const newAllRequests = repos.reduce((requests, { user, repo }) => {
+          return newRequests(requests, newLabelRequest({ user, repo, token }));
+        }, requests);
+        return assign({}, state, { requests: newAllRequests });
+      }),
+    updateLabel$
+      .map(label => state => {
+        const { labels } = state;
+        const index = labels.indexOf(label);
+        if (index >= 0) return state;
+        const newLabels = labels.concat([label]);
+        return assign({}, state, { labels: newLabels });
+      })
+  );
 }
 
 export default function(actions) {
