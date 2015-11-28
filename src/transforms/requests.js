c88c29816523ff8f8a617b60b8949cc352dfec67
bouzuya <m@bouzuya.net>
2015-11-28 12:44:25 +0900

    extract requests transforms

---
+++ src/transforms/requests.js
@@ -0,0 +1,55 @@
+import Rx from 'rx';
+import assign from '../utils/assign';
+import newRequests from '../utils/new-requests';
+
+function newIssueRequest({ user, repo, token }) {
+  const url = `https://api.github.com/repos/${user}/${repo}/issues`;
+  const ua = { 'User-Agent': 'gh-tree' };
+  const auth = token ? { 'Authorization': `token ${token}` } : {};
+  const headers = assign({}, ua, auth);
+  return { method: 'GET', url, headers };
+}
+
+function newLabelRequest({ user, repo, token }) {
+  const url = `https://api.github.com/repos/${user}/${repo}/labels`;
+  const ua = { 'User-Agent': 'gh-tree' };
+  const auth = token ? { 'Authorization': `token ${token}` } : {};
+  const headers = assign({}, ua, auth);
+  return { method: 'GET', url, headers };
+}
+
+function fetchIssuesTransform({ fetchIssues$ }) {
+  return fetchIssues$
+    .map(() => state => {
+      const { settings, requests } = state;
+      const repos = settings && settings.repos ? settings.repos : [];
+      const token = settings.token;
+      const newAllRequests = repos.reduce((requests, { user, repo }) => {
+        return newRequests(requests, newIssueRequest({ user, repo, token }));
+      }, requests);
+      // NOTE: reset issues
+      return assign({}, state, { issues: [], requests: newAllRequests });
+    });
+}
+
+function fetchLabelsTransform({ fetchLabels$ }) {
+  return fetchLabels$
+    .map(() => state => {
+      const { settings, requests } = state;
+      const repos = settings && settings.repos ? settings.repos : [];
+      const token = settings.token;
+      const newAllRequests = repos.reduce((requests, { user, repo }) => {
+        return newRequests(requests, newLabelRequest({ user, repo, token }));
+      }, requests);
+      // NOTE: reset labels
+      return assign({}, state, { labels: [], requests: newAllRequests });
+    });
+}
+
+export default function labels(actions) {
+  // NOTE: no namespace
+  return Rx.Observable.merge(
+    fetchIssuesTransform(actions),
+    fetchLabelsTransform(actions)
+  );
+}
