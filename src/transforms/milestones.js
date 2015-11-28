4debffbd2e43282b81f07b258968415e9671db44
bouzuya <bouzuya@storm.local>
2015-11-28 21:29:39 +0900

    add transforms for milestone action

---
+++ src/transforms/milestones.js
@@ -0,0 +1,22 @@
+import { Observable } from 'rx';
+import transform from '../utils/transform';
+
+function fetchMilestonesTransform({ fetchMilestones$ }) {
+  return fetchMilestones$.map(transform(() => ({ milestones: [] })));
+}
+
+function updateMilestoneTransform({ updateMilestone$ }) {
+  return updateMilestone$.map(transform(({ milestones }, milestone) => {
+    if (milestones.indexOf(milestone) < 0) {
+      return { milestones: milestones.concat([milestone]) };
+    }
+  }));
+}
+
+export default function(actions) {
+  // NOTE: no namespace
+  return Observable.merge(
+    fetchMilestonesTransform(actions),
+    updateMilestoneTransform(actions)
+  );
+}
