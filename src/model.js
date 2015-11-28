61adee04f395881f0fab5ac68b7b6331689823e2
bouzuya <bouzuya@storm.local>
2015-11-28 12:33:53 +0900

    extract labels

--- src/model.js
+++ src/model.js
@@ -1,9 +1,13 @@
 import Rx from 'rx';
 import issues from './transforms/issues';
+import labels from './transforms/labels';
 import repos from './transforms/repos';
 import token from './transforms/token';
 import assign from './utils/assign';
-import newRequests from './utils/new-requests';
+
+function initializeLabels() {
+  return { labels: [] };
+}
 
 function initializeRepos() {
   return {
@@ -15,6 +19,7 @@ function initializeRepos() {
     }
   };
 }
+
 function initializeToken() {
   return {
     // TODO: settings.token: 'xxx'
@@ -23,38 +28,6 @@ function initializeToken() {
       value: ''
     }
   };
-}
-
-function newLabelRequest({ user, repo, token }) {
-  const url = `https://api.github.com/repos/${user}/${repo}/labels`;
-  const ua = { 'User-Agent': 'gh-tree' };
-  const auth = token ? { 'Authorization': `token ${token}` } : {};
-  const headers = assign({}, ua, auth);
-  return { method: 'GET', url, headers };
-}
-
-function labels(actions) {
-  const { updateLabel$, fetchLabels$ } = actions;
-  return Rx.Observable.merge(
-    fetchLabels$
-      .map(() => state => {
-        const { settings, requests } = state;
-        const repos = settings && settings.repos ? settings.repos : [];
-        const token = settings.token;
-        const newAllRequests = repos.reduce((requests, { user, repo }) => {
-          return newRequests(requests, newLabelRequest({ user, repo, token }));
-        }, requests);
-        return assign({}, state, { requests: newAllRequests });
-      }),
-    updateLabel$
-      .map(label => state => {
-        const { labels } = state;
-        const index = labels.indexOf(label);
-        if (index >= 0) return state;
-        const newLabels = labels.concat([label]);
-        return assign({}, state, { labels: newLabels });
-      })
-  );
 }
 
 export default function(actions) {
@@ -68,9 +41,9 @@ export default function(actions) {
         { type: 'label', name: 'ready' }
       ],
       issues: [],
-      labels: [],
       requests: []
     },
+    initializeLabels(),
     initializeRepos(),
     initializeToken()
   );
